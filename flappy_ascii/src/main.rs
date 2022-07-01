a8d736cd90342e945cf4f1453008f6b136426c8c
Xipho <xipho@mail.ru>
2022-07-01 22:05:55 +0500

    Added constants

--- flappy_ascii/src/main.rs
+++ flappy_ascii/src/main.rs
@@ -1,5 +1,9 @@
 use bracket_lib::prelude::*;
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
@@ -9,18 +13,24 @@ fn main() -> BError {
 }
 
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
 impl State {
     fn new() -> Self {
         Self {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu
         }
     }
 
     fn restart(&mut self) {
-        self.mode = GameMode::Playing
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
+        self.mode = GameMode::Playing;
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -98,5 +108,20 @@ impl Player {
             BLACK,
             to_cp437('@')
         );
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
     }
 }
