dc753f9b138bee1451aa77c96531db2de01cbb01
Xipho <xipho@mail.ru>
2022-08-08 22:58:35 +0500

    added game over and winning systems

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -43,6 +43,7 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
+        spawn_amulet_of_yala(&mut ecs, map_builder.amulet_start);
         map_builder
             .rooms
             .iter()
@@ -68,15 +69,13 @@ impl State {
             4,
             WHITE,
             BLACK,
-            "Slain by a monster, your hero's journey has come to a \
-            premature end",
+            "Slain by a monster, your hero's journey has come to a premature end",
         );
         ctx.print_color_centered(
             5,
             WHITE,
             BLACK,
-            "The Amulet of Yala remains unclaimed, and your home town \
-            is not saved",
+            "The Amulet of Yala remains unclaimed, and your home town is not saved",
         );
         ctx.print_color_centered(
             8,
@@ -91,20 +90,51 @@ impl State {
             "Press 1 to play again.",
         );
         if let Some(VirtualKeyCode::Key1) = ctx.key {
-            self.ecs = World::default();
-            self.resources = Resources::default();
-            let mut rng = RandomNumberGenerator::new();
-            let map_builder = MapBuilder::new(&mut rng);
-            spawn_player(&mut self.ecs, map_builder.player_start);
-            map_builder.rooms
-                .iter()
-                .skip(1)
-                .map(|r| r.center())
-                .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
-            self.resources.insert(map_builder.map);
-            self.resources.insert(Camera::new(map_builder.player_start));
-            self.resources.insert(TurnState::AwaitingInput);
+            self.reset_game_state();
         }
+    }
+
+    fn victory(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(2);
+        ctx.print_color_centered(2, GREEN, BLACK, "You have won!");
+        ctx.print_color_centered(
+            4,
+            WHITE,
+            BLACK,
+            "You put on the Amulet of Yala and feel its power course through your veins.",
+        );
+        ctx.print_color_centered(
+            5,
+            WHITE,
+            BLACK,
+            "Your town is saved, and you can return to your normal life.",
+        );
+        ctx.print_color_centered(
+            7,
+            YELLOW,
+            BLACK,
+            "Press 1 to play again.",
+        );
+        if let Some(VirtualKeyCode::Key1) = ctx.key {
+            self.reset_game_state();
+        }
+    }
+
+    fn reset_game_state(&mut self) {
+        self.ecs = World::default();
+        self.resources = Resources::default();
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+        spawn_player(&mut self.ecs, map_builder.player_start);
+        spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
+        map_builder.rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
+        self.resources.insert(map_builder.map);
+        self.resources.insert(Camera::new(map_builder.player_start));
+        self.resources.insert(TurnState::AwaitingInput);
     }
 }
 
@@ -120,18 +150,14 @@ impl GameState for State {
         ctx.set_active_console(0);
         self.resources.insert(Point::from_tuple(ctx.mouse_pos()));
         let current_state = self.resources.get::<TurnState>().unwrap().clone();
+        let ecs = &mut self.ecs;
+        let res = &mut self.resources;
         match current_state {
-            TurnState::AwaitingInput => self
-                .input_systems
-                .execute(&mut self.ecs, &mut self.resources),
-            TurnState::PlayerTurn => self
-                .player_systems
-                .execute(&mut self.ecs, &mut self.resources),
-            TurnState::MonsterTurn => self
-                .monster_systems
-                .execute(&mut self.ecs, &mut self.resources),
-            TurnState::GameOver =>
-                self.game_over(ctx)
+            TurnState::AwaitingInput => self.input_systems.execute(ecs, res),
+            TurnState::PlayerTurn => self.player_systems.execute(ecs, res),
+            TurnState::MonsterTurn => self.monster_systems.execute(ecs, res),
+            TurnState::GameOver => self.game_over(ctx),
+            TurnState::Victory => self.victory(ctx)
         }
         render_draw_buffer(ctx).expect("Render error");
     }
--- dungeoncrawl/src/spawner.rs
+++ dungeoncrawl/src/spawner.rs
@@ -14,6 +14,19 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
     );
 }
 
+pub fn spawn_amulet_of_yala(ecs: &mut World, pos: Point) {
+    ecs.push(
+        (
+            Item, AmuletOfYala, pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph: to_cp437('|')
+            },
+            Name("Amulet of Yala".to_string())
+        )
+    );
+}
+
 pub fn spawn_monster(
     ecs: &mut World,
     rng: &mut RandomNumberGenerator,
