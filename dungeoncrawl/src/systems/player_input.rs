5c1896208b147e8e827847b5d1e5b4a55a2888d8
Xipho <xipho@mail.ru>
2022-09-04 14:45:19 +0500

    removed heal on wait

--- dungeoncrawl/src/systems/player_input.rs
+++ dungeoncrawl/src/systems/player_input.rs
@@ -72,14 +72,7 @@ pub fn player_input(
                 }));
             }
         }
-        if !did_something {
-            if let Ok(mut health) = ecs
-                .entry_mut(player_entity)
-                .unwrap()
-                .get_component_mut::<Health>() {
-                health.current = i32::min(health.max, health.current + 1);
-            }
-        }
+        
         *turn_state = TurnState::PlayerTurn
     }
 }
