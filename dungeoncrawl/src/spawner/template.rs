0a9da08b9e88158c9e6ab17d321053961fad0664
Xipho <xipho@mail.ru>
2022-10-15 13:10:18 +0500

    Added damage calculations to combat system

--- dungeoncrawl/src/spawner/template.rs
+++ dungeoncrawl/src/spawner/template.rs
@@ -13,7 +13,8 @@ pub struct Template {
     pub name: String,
     pub glyph: char,
     pub provides: Option<Vec<(String, i32)>>,
-    pub hp: Option<i32>
+    pub hp: Option<i32>,
+    pub base_damage: Option<i32>
 }
 
 #[derive(Clone, Deserialize, Debug, PartialEq)]
@@ -95,6 +96,12 @@ impl Templates {
                     }
                 }
             });
+        }
+        if let Some(damage) = &template.base_damage {
+            commands.add_component(entity, Damage(*damage));
+            if template.entity_type == EntityType::Item {
+                commands.add_component(entity, Weapon{});
+            }
         }
     }
 }
